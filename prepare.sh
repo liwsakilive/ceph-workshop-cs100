6d740bb63919160c35d55822f81956a368315bcc
root <root@ceph-workshop-1.local>
2020-01-28 16:48:40 +0700

    add prepare script

---
+++ prepare.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/liwsakilive/ceph-workshop-cs100.git
+sudo add-apt-repository ppa:ansible/ansible-2.4
+sudo apt-get update -y && apt-get install ansible python-pip python-netaddr -y
